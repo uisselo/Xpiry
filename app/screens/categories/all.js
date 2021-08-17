2994067a327425dcae793b18f45ff07143a923bf
Carl <65150183+cndagangon@users.noreply.github.com>
2021-08-17 22:44:07 +0800

    not sure if magwwork

--- app/screens/categories/all.js
+++ app/screens/categories/all.js
@@ -9,6 +9,13 @@ function all(props) {
   );
 }
 
+ // input items to search
+<SearchBar
+  placeholder="Search here"
+  onPress={() => alert("onPress")}
+  onChangeText={(text) => console.log(text)}
+/>
+
 const styles = StyleSheet.create({
   container: {
     flex: 1,
@@ -18,6 +25,8 @@ const styles = StyleSheet.create({
     justifyContent: "center",
     flexWrap: "wrap",
     paddingTop: 50,
+
+    
   },
 });
 
