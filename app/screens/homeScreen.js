718959a4a5c50a45b947f1ad585ee9a8f9cf8835
louisse <louisse.evale.iics@ust.edu.ph>
2021-08-17 20:27:35 +0800

    konting change lang

--- app/screens/homeScreen.js
+++ app/screens/homeScreen.js
@@ -4,6 +4,10 @@ import { StyleSheet, Text, View, TouchableOpacity, Image } from "react-native";
 function homeScreen({ navigation }) {
   return (
     <View style={[styles.container]}>
+      <View style={[styles.welcome]}>
+        <Text style={{ fontSize: 20 }}>Welcome</Text>
+        <Text style={{ fontSize: 30 }}>Nunito Sans</Text>
+      </View>
       <TouchableOpacity // all items
         style={[
           styles.box,
@@ -123,6 +127,10 @@ const styles = StyleSheet.create({
     fontWeight: "bold",
     position: "absolute",
     top: 0,
+  },
+  welcome: {
+    width: 350,
+    paddingBottom: 15,
   },
   icon: {
     width: 70,
