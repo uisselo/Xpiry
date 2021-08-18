15fbbf84bae474d2580192194648959b26382d68
Carl <65150183+cndagangon@users.noreply.github.com>
2021-08-18 22:03:04 +0800

    search bar try lang ulit

--- app/screens/categories/all.js
+++ app/screens/categories/all.js
@@ -1,33 +1,55 @@
-import React from "react";
-import { StyleSheet, Text, View, SafeAreaView, Image } from "react-native";
+import React, { Component } from 'react';
+import { StyleSheet, Text, View, ScrollView, TouchableOpacity } from 'react-native';
+import SearchInput, { createFilter } from 'react-native-search-filter';
+import emails from './emails';
+const KEYS_TO_FILTERS = ['user.name', 'subject'];
 
-function all(props) {
-  return (
-    <View style={[styles.container]}>
-      <Text>All Items</Text>
-    </View>
-  );
+export default class App extends Component {
+ constructor(props) {
+    super(props);
+    this.state = {
+      searchTerm: ''
+    }
+  }
+  searchUpdated(term) {
+    this.setState({ searchTerm: term })
+  }
+  render() {
+    const filteredEmails = emails.filter(createFilter(this.state.searchTerm, KEYS_TO_FILTERS))
+    return (
+      <View style={styles.container}>
+        <SearchInput 
+          onChangeText={(term) => { this.searchUpdated(term) }} 
+          style={styles.searchInput}
+          placeholder="Type a message to search"
+          />
+        <ScrollView>
+          {filteredEmails.map(email => {
+            return (
+              <TouchableOpacity onPress={()=>alert(email.user.name)} key={email.id} style={styles.emailItem}>
+                <View>
+                  <Text>{email.user.name}</Text>
+                  <Text style={styles.emailSubject}>{email.subject}</Text>
+                </View>
+              </TouchableOpacity>
+            )
+          })}
+        </ScrollView>
+      </View>
+    );
+  }
 }
-
- // input items to search
-<SearchBar
-  placeholder="Search here"
-  onPress={() => alert("onPress")}
-  onChangeText={(text) => console.log(text)}
-/>
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#fff",
-    justifyContent: "center",
-    flexDirection: "row",
-    justifyContent: "center",
-    flexWrap: "wrap",
-    paddingTop: 50,
-
-    
+    backgroundColor: '#fff',
+    justifyContent: 'flex-start'
   },
+  searchInput:{
+    padding: 10,
+    borderColor: '#CCC',
+    borderWidth: 1
+  }
 });
-
-export default all;
+  export default all;
