32a68dec0b72ee703921f0b0d1763136c1a8b821
louisse <louisse.evale.iics@ust.edu.ph>
2021-11-05 18:53:15 +0800

    smaller data number font size

--- app/screens/dashboard.js
+++ app/screens/dashboard.js
@@ -788,7 +788,7 @@ const styles = StyleSheet.create({
     color: "#fff",
   },
   dataNum: {
-    fontSize: widthPercentageToDP(18),
+    fontSize: widthPercentageToDP(15),
     fontFamily: "Montserrat-Bold",
     color: "#fff",
   },
